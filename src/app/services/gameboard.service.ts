fa9e3fe66b380f1271cf92bcb7a354ee5dbf9d25
Jacquelyn Gboyor <jacquelyngboyor@gmail.com>
2023-06-04 01:51:37 -0400

    create category & question models

--- src/app/services/gameboard.service.ts
+++ src/app/services/gameboard.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import categoryJson from '../data/game-questions.json';
+
+export interface IQuestion {
+  question: string;
+  points: number;
+  answer: string;
+  options: string[];
+}
+
+export interface ICategory {
+  category: string;
+  questions: IQuestion[];
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameboardService {
 
+  categories: ICategory[] = categoryJson;
+
   constructor() { }
 
+  getGameQuestions(): ICategory[] {
+    return this.categories;
+  }
 }
