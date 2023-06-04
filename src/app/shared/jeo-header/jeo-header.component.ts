a70d017df96f24027134d9401ad8885ff49ed0e2
Jacquelyn Gboyor <jacquelyngboyor@gmail.com>
2023-06-04 08:26:25 -0400

    Update jeo-header to be theme-able. Make jeo-header more dynamic.

--- src/app/shared/jeo-header/jeo-header.component.ts
+++ src/app/shared/jeo-header/jeo-header.component.ts
@@ -1,23 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+
+type headerTheme = 'primary' | 'warning' | 'danger' | 'dark';
+
+enum HeaderThemeClass {
+  'primary' = '',
+  'warning' = 'warning-header',
+  'danger' = 'danger-header',
+  'dark' = 'dark-header'
+}
 
 @Component({
   selector: 'jeo-header',
   template: `
-  <div class="jeo-header">
+  <div class="jeo-header {{getThemeClass}}">
     <div class="jeo-header-line"></div>
     <div class="jeo-header-title">
-      <h4 class="jeo-header-text">{{labelText}}</h4>
+      <h4 class="jeo-header-text" [innerHTML]="labelText"></h4>
     </div>
   </div>
   `
 })
-export class JeoHeaderComponent implements OnInit {
+export class JeoHeaderComponent {
 
   @Input() labelText: string = '';
+  @Input() theme: headerTheme = 'primary';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get getThemeClass(): string {
+    return HeaderThemeClass[this.theme];
   }
+
 
 }
